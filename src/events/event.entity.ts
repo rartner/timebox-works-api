33519486800a3ba49e8b6de07707c6bf99122377
Ramon Artner <ramon.artner@gmail.com>
2020-08-08 03:41:25 -0300

    database configs, creating first entity

---
+++ src/events/event.entity.ts
@@ -0,0 +1,39 @@
+import {
+	BaseEntity,
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	CreateDateColumn,
+	UpdateDateColumn
+} from 'typeorm';
+
+@Entity()
+export class Event extends BaseEntity {
+
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column({ nullable: false, type: 'varchar', length: 100 })
+	name: string;
+
+	@Column({ nullable: false, type: 'varchar', length: 500 })
+	description: string;
+
+	@Column({ name: 'expected_start_date', nullable: false, type: 'timestamp' })
+	expectedStartDate: Date;
+
+	@Column({ name: 'expected_end_date', nullable: false, type: 'timestamp' })
+	expectedEndDate: Date;
+
+	@Column({ name: 'real_start_date', type: 'timestamp' })
+	realStartDate: Date;
+
+	@Column({ name: 'real_end_date', type: 'timestamp' })
+	realEndDate: Date;
+
+	@CreateDateColumn({ name: 'created_at' })
+	createdAt: Date;
+
+	@UpdateDateColumn({ name: 'updated_at' })
+	updatedAt: Date;
+}
